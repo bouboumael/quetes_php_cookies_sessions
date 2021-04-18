03e3fda730dc5b95038015cf2f50efdd9f306bcf
bouboumael <bouboumael@gmail.com>
2021-04-18 13:34:19 +0200

    start session cart

--- cart.php
+++ cart.php
@@ -1,4 +1,12 @@
-<?php require 'inc/head.php'; ?>
+<?php
+require 'inc/head.php';
+require_once 'inc/data/products.php'
+?>
+<?php
+if (!isset($_SESSION['user'])) {
+    header('location: /');
+}
+?>
 <section class="cookies container-fluid">
     <div class="row">
         TODO : Display shopping cart items from $_SESSION here.
--- login.php
+++ login.php
@@ -1,4 +1,17 @@
-<?php require 'inc/head.php'; ?>
+<?php
+require 'inc/head.php';
+
+if (isset($_SESSION['user'])) {
+    header('location: /');
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $form = $_POST;
+    $_SESSION['user'] = $form['loginname'];
+    header('location: /');
+}
+
+?>
 <div class="container" style="margin-top:40px">
     <div class="row">
         <div class="col-sm-6 col-md-4 col-md-offset-4">
@@ -11,9 +24,7 @@
                         <fieldset>
                             <div class="row">
                                 <div class="center-block">
-                                    <img class="profile-img"
-                                         src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
-                                         alt="">
+                                    <img class="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120" alt="">
                                 </div>
                             </div>
                             <div class="row">
@@ -21,10 +32,9 @@
                                     <div class="form-group">
                                         <div class="input-group">
                                             <span class="input-group-addon">
-                                              <i class="glyphicon glyphicon-user"></i>
+                                                <i class="glyphicon glyphicon-user"></i>
                                             </span>
-                                            <input class="form-control" placeholder="Username" name="loginname"
-                                                   type="text" autofocus>
+                                            <input class="form-control" placeholder="Username" name="loginname" type="text" autofocus>
                                         </div>
                                     </div>
                                     <div class="form-group">
